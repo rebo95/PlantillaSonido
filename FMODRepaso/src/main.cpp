148156d01d671312669b963ce98d811f372abefc
rebo95 <arebored@ucm.es>
2020-01-09 22:49:24 +0100

    Plantilla sonido updated

--- FMODRepaso/src/main.cpp
+++ FMODRepaso/src/main.cpp
@@ -18,36 +18,6 @@ FMOD_RESULT result = FMOD_RESULT::FMOD_OK;
 
 int main() {
 
-	//FMOD::Studio::System* _studioSystem = NULL;
-	//FMOD::Studio::System::create(&_studioSystem);
-
-	//ERRCHECK(_studioSystem->initialize(1024, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, 0));
-
-	//FMOD::Studio::Bank* masterBank = NULL;
-	//ERRCHECK(_studioSystem->loadBankFile("res/MasterBank.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &masterBank));
-
-	//FMOD::Studio::Bank* stringsBank = NULL;
-	//ERRCHECK(_studioSystem->loadBankFile("res/MasterBank.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &stringsBank));
-
-	//FMOD::Studio::Bank* stepsBank = NULL;
-	//ERRCHECK(_studioSystem->loadBankFile("res/StepsBank.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &stepsBank));
-
-	//// Get the Looping Ambience event
-	//FMOD::Studio::EventDescription* pasos = NULL;
-	//ERRCHECK(_studioSystem->getEvent("event:/Steps", &pasos));
-
-	//FMOD::Studio::EventInstance* pasosInstance = NULL;
-	//ERRCHECK(pasos->createInstance(&pasosInstance));
-
-	//ERRCHECK(pasosInstance->start());
-
-	//ERRCHECK(pasosInstance->setParameterByName("velocidad", 0.1));
-
-	//do
-	//{
-	//	_studioSystem->update();
-	//} while (true);
-
 	//---------- Inicialización de System ----------//
 // Creamos System
 	FMOD::System* FMODSystem;
@@ -80,3 +50,34 @@ int main() {
 
 	return 0;
 }
+
+
+//FMOD::Studio::System* _studioSystem = NULL;
+//FMOD::Studio::System::create(&_studioSystem);
+
+//ERRCHECK(_studioSystem->initialize(1024, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, 0));
+
+//FMOD::Studio::Bank* masterBank = NULL;
+//ERRCHECK(_studioSystem->loadBankFile("res/MasterBank.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &masterBank));
+
+//FMOD::Studio::Bank* stringsBank = NULL;
+//ERRCHECK(_studioSystem->loadBankFile("res/MasterBank.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &stringsBank));
+
+//FMOD::Studio::Bank* stepsBank = NULL;
+//ERRCHECK(_studioSystem->loadBankFile("res/StepsBank.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &stepsBank));
+
+//// Get the Looping Ambience event
+//FMOD::Studio::EventDescription* pasos = NULL;
+//ERRCHECK(_studioSystem->getEvent("event:/Steps", &pasos));
+
+//FMOD::Studio::EventInstance* pasosInstance = NULL;
+//ERRCHECK(pasos->createInstance(&pasosInstance));
+
+//ERRCHECK(pasosInstance->start());
+
+//ERRCHECK(pasosInstance->setParameterByName("velocidad", 0.1));
+
+//do
+//{
+//	_studioSystem->update();
+//} while (true);
